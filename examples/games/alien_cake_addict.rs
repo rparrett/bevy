6824437bef1157b228213b121a1af1cc52f50d14
Rob Parrett <robparrett@gmail.com>
2023-01-17 14:36:56 -0700

    Split camera-related values into separate resource

--- examples/games/alien_cake_addict.rs
+++ examples/games/alien_cake_addict.rs
@@ -17,6 +17,7 @@ struct BonusSpawnTimer(Timer);
 fn main() {
     App::new()
         .init_resource::<Game>()
+        .init_resource::<CameraFocus>()
         .insert_resource(BonusSpawnTimer(Timer::from_seconds(
             5.0,
             TimerMode::Repeating,
@@ -68,6 +69,10 @@ struct Game {
     bonus: Bonus,
     score: i32,
     cake_eaten: u32,
+}
+
+#[derive(Resource, Default)]
+struct CameraFocus {
     camera_should_focus: Vec3,
     camera_is_focus: Vec3,
 }
@@ -81,16 +86,16 @@ const RESET_FOCUS: Vec3 = Vec3::new(
     BOARD_SIZE_J as f32 / 2.0 - 0.5,
 );
 
-fn setup_cameras(mut commands: Commands, mut game: ResMut<Game>) {
-    game.camera_should_focus = RESET_FOCUS;
-    game.camera_is_focus = game.camera_should_focus;
+fn setup_cameras(mut commands: Commands, mut camera_focus: ResMut<CameraFocus>) {
+    camera_focus.camera_should_focus = RESET_FOCUS;
+    camera_focus.camera_is_focus = camera_focus.camera_should_focus;
     commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(
             -(BOARD_SIZE_I as f32 / 2.0),
             2.0 * BOARD_SIZE_J as f32 / 3.0,
             BOARD_SIZE_J as f32 / 2.0 - 0.5,
         )
-        .looking_at(game.camera_is_focus, Vec3::Y),
+        .looking_at(camera_focus.camera_is_focus, Vec3::Y),
         ..default()
     });
 }
@@ -253,7 +258,8 @@ fn move_player(
 // change the focus of the camera
 fn focus_camera(
     time: Res<Time>,
-    mut game: ResMut<Game>,
+    game: Res<Game>,
+    mut camera_focus: ResMut<CameraFocus>,
     mut transforms: ParamSet<(Query<&mut Transform, With<Camera3d>>, Query<&Transform>)>,
 ) {
     const SPEED: f32 = 2.0;
@@ -264,31 +270,31 @@ fn focus_camera(
             transform_query.get(player_entity),
             transform_query.get(bonus_entity),
         ) {
-            game.camera_should_focus = player_transform
+            camera_focus.camera_should_focus = player_transform
                 .translation
                 .lerp(bonus_transform.translation, 0.5);
         }
         // otherwise, if there is only a player, target the player
     } else if let Some(player_entity) = game.player.entity {
         if let Ok(player_transform) = transforms.p1().get(player_entity) {
-            game.camera_should_focus = player_transform.translation;
+            camera_focus.camera_should_focus = player_transform.translation;
         }
         // otherwise, target the middle
     } else {
-        game.camera_should_focus = RESET_FOCUS;
+        camera_focus.camera_should_focus = RESET_FOCUS;
     }
     // calculate the camera motion based on the difference between where the camera is looking
     // and where it should be looking; the greater the distance, the faster the motion;
     // smooth out the camera movement using the frame time
-    let mut camera_motion = game.camera_should_focus - game.camera_is_focus;
+    let mut camera_motion = camera_focus.camera_should_focus - camera_focus.camera_is_focus;
     if camera_motion.length() > 0.2 {
         camera_motion *= SPEED * time.delta_seconds();
         // set the new camera's actual focus
-        game.camera_is_focus += camera_motion;
+        camera_focus.camera_is_focus += camera_motion;
     }
     // look at that new camera's actual focus
     for mut transform in transforms.p0().iter_mut() {
-        *transform = transform.looking_at(game.camera_is_focus, Vec3::Y);
+        *transform = transform.looking_at(camera_focus.camera_is_focus, Vec3::Y);
     }
 }
 
