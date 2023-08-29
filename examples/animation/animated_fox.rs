d0b6cd72eab0273f6b60ad8d09eedf634bf97879
Rob Parrett <robparrett@gmail.com>
2023-08-28 19:24:49 -0700

    Revert "merge imports"
    This reverts commit 8a2f3ca1778087220b6a22d1465e360c7e88aae9.

--- examples/animation/animated_fox.rs
+++ examples/animation/animated_fox.rs
@@ -3,7 +3,9 @@
 use std::f32::consts::PI;
 use std::time::Duration;
 
-use bevy::{animation::RepeatAnimation, pbr::CascadeShadowConfigBuilder, prelude::*};
+use bevy::animation::RepeatAnimation;
+use bevy::pbr::CascadeShadowConfigBuilder;
+use bevy::prelude::*;
 
 fn main() {
     App::new()
